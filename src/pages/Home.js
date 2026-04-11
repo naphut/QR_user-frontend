99f05b1a9f9a0be76d11fa15077bb74e2f9e0041
RET Naphut <retnaphut@RETs-MacBook-Air.local>
2026-04-11 21:05:38 +0700

    Fix ESLint error - restore useState import needed by ProductCard component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useProducts } from '../context/ProductContext';
 import { useWishlist } from '../context/WishlistContext';
